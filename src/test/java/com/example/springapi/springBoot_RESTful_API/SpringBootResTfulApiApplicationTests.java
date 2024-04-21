683fb2f365323226b116605b1ea74156cea25c8c
Nikhil Gowda <Nick3344@users.noreply.github.com>
2024-04-22 00:46:29 +0200

    This SpringBoot project demonstrates the implementation of a simple RESTful API for managing user data. It includes endpoints to retrieve user information based on their ID.

---
+++ src/test/java/com/example/springapi/springBoot_RESTful_API/SpringBootResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapi.springBoot_RESTful_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
